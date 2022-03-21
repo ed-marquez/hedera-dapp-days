4b35fad2f4b3cf55bd704f33e446ce6dfa2a0ddb
ed-marquez <ed.marquez@hedera.com>
2022-03-21 16:43:39 -0400

    Starting point

--- src/components/hedera/tokenCreate.js
+++ src/components/hedera/tokenCreate.js
@@ -1,43 +1,10 @@
 import React from "react";
 import operator from "../../config.js";
-import {
-	Client,
-	AccountId,
-	PrivateKey,
-	TokenCreateTransaction,
-	TokenMintTransaction,
-	FileCreateTransaction,
-	ContractCreateTransaction,
-	ContractFunctionParameters,
-	ContractExecuteTransaction,
-	TokenInfoQuery,
-	AccountBalanceQuery,
-	Hbar,
-	ContractInfoQuery,
-} from "@hashgraph/sdk";
+import { Client, AccountId, PrivateKey, TokenCreateTransaction } from "@hashgraph/sdk";
 
 async function tokenCreateFcn() {
-	const operatorId = AccountId.fromString(operator.id);
-	const operatorKey = PrivateKey.fromString(operator.pvkey);
-	const client = Client.forTestnet().setOperator(operatorId, operatorKey);
-
-	console.log("- Creating token");
-
-	const tokenCreateTx = new TokenCreateTransaction()
-		.setTokenName("dAppDayToken")
-		.setTokenSymbol("DDT")
-		.setTreasuryAccountId(operatorId)
-		.setInitialSupply(100)
-		.setDecimals(0)
-		.setSupplyKey(operatorKey)
-		.freezeWith(client);
-	const tokenCreateSign = await tokenCreateTx.sign(operatorKey);
-	const tokenCreateSubmit = await tokenCreateSign.execute(client);
-	const tokenCreateRec = await tokenCreateSubmit.getRecord(client);
-	const tId = tokenCreateRec.receipt.tokenId;
-	const supply = tokenCreateTx._initialSupply.low;
-
-	return [tId, supply];
+	//Configure the Hedera client...
+	//Add code to create the HTS token...
 }
 
 export default tokenCreateFcn;
