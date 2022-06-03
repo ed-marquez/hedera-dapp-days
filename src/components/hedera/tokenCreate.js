bdb0d4e14eac5e1fe01f23ad7deee2775eb93a72
ed-marquez <72571340+ed-marquez@users.noreply.github.com>
2022-06-03 16:05:59 -0400

    starting point ready

--- src/components/hedera/tokenCreate.js
+++ src/components/hedera/tokenCreate.js
@@ -10,27 +10,7 @@ async function tokenCreateFcn(walletData, accountId) {
 	const provider = hashconnect.getProvider("testnet", saveData.topic, accountId);
 	const signer = hashconnect.getSigner(provider);
 
-	const url = `https://testnet.mirrornode.hedera.com/api/v1/accounts?account.id=${accountId}`;
-	const mirrorQuery = await axios(url);
-	const supplyKey = PublicKey.fromString(mirrorQuery.data.accounts[0].key.key);
-
-	const tokenCreateTx = await new TokenCreateTransaction()
-		.setTokenName("dAppDayToken")
-		.setTokenSymbol("DDT")
-		.setTreasuryAccountId(accountId)
-		.setAutoRenewAccountId(accountId)
-		.setAutoRenewPeriod(7776000)
-		.setInitialSupply(400)
-		.setDecimals(0)
-		.setSupplyKey(supplyKey)
-		.freezeWithSigner(signer);
-	const tokenCreateSubmit = await tokenCreateTx.executeWithSigner(signer);
-	const tokenCreateRx = await provider.getTransactionReceipt(tokenCreateSubmit.transactionId);
-	const tId = tokenCreateRx.tokenId;
-	const supply = tokenCreateTx._initialSupply.low;
-	console.log(`- Created HTS token with ID: ${tId}`);
-
-	return [tId, supply, tokenCreateSubmit.transactionId];
+	// Create new HTS fungible token
 }
 
 export default tokenCreateFcn;
