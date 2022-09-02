e3686aafd3f7b9c58e6e6e6d8ad6f93cbf6e242f
eyargholder <eyargholder@gmail.com>
2022-09-02 14:15:08 -0500

    Update tokenCreate.js
    Initial Supply from 1000 TO READ 0

--- src/components/hedera/tokenCreate.js
+++ src/components/hedera/tokenCreate.js
@@ -20,7 +20,7 @@ async function tokenCreateFcn(walletData, accountId) {
 		.setTreasuryAccountId(accountId)
 		.setAutoRenewAccountId(accountId)
 		.setAutoRenewPeriod(7776000)
-		.setInitialSupply(1000)
+		.setInitialSupply(0)
 		.setDecimals(0)
 		.setSupplyKey(supplyKey)
 		.freezeWithSigner(signer);
